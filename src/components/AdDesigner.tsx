c12a80675012e0a026462f28ce958e1123b78339
suhalder <subhobrata.besu@gmail.com>
2022-10-03 18:08:23 -0400

    commit on 10/3

--- src/components/AdDesigner.tsx
+++ src/components/AdDesigner.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
 import "./AdDesigner.css";
+
 const AdDesigner = () => {
+  const [number, setNumber] = useState(0); //Variable
+
+  function countUp() {
+    setNumber((prev) => prev + 1);
+  }
+
+  function countDown() {
+    setNumber((prev) => prev - 1);
+  }
+
   return (
     <div className="AdDesigner">
       <h1>Ad Designer</h1>
@@ -7,41 +19,23 @@ const AdDesigner = () => {
       <p>
         <b>What to support</b>
       </p>
-      <nav>
-        <a href="#" className="button">
-          Chocolate
-        </a>
-        <a href="#" className="button">
-          Vanilla
-        </a>
-        <a href="#" className="button">
-          Strawberry
-        </a>
-      </nav>
-
+      <button>Chocolate</button>
+      &nbsp;&nbsp;&nbsp;
+      <button>Vanilla</button>
+      &nbsp;&nbsp;&nbsp;
+      <button>Strawberry</button>
       <p>
         <b>Color Theme</b>
       </p>
-      <nav>
-        <a href="#" className="button">
-          Light
-        </a>
-        <a href="#" className="button">
-          Dark
-        </a>
-      </nav>
-
+      <button>Light</button>
+      &nbsp;&nbsp;&nbsp;
+      <button>Dark</button>
       <p>
         <b>Font Size</b>
       </p>
-      <nav>
-        <a href="#" className="button">
-          Down
-        </a>
-        <a href="#" className="button">
-          Up
-        </a>
-      </nav>
+      <button onClick={() => countDown()}>Down</button>
+      &nbsp;&nbsp;&nbsp;{number}&nbsp;&nbsp;&nbsp;
+      <button onClick={() => countUp()}>Up</button>
     </div>
   );
 };
---
+++ src/components/VoteHere.tsx
@@ -0,0 +1,16 @@
+import "./VoteHere.css";
+
+const VoteHere = () => {
+  return (
+    <div className="VoteHere">
+      <h1>Vote Here</h1>
+      <button>Chocolate</button>
+      &nbsp;&nbsp;&nbsp;
+      <button>Vanilla</button>
+      &nbsp;&nbsp;&nbsp;
+      <button>Strawberry</button>
+    </div>
+  );
+};
+
+export default VoteHere;
